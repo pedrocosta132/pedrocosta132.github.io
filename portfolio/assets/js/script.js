030c549256d108731e66a984ff36f21f9ae4c946
pedrocosta132 <pedroarcosta99@gmail.com>
2020-02-24 19:30:26 +0000

    Smooth Scroll

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -6,6 +6,8 @@ $(function(){
     //Make necessary scroll changes on page load
     scrollChanges($(window).scrollTop());
 
+    //----------LISTENERS----------
+
     //Add animation delay to nav links dynamically
     $("header > nav > ul > li").each(function(index){
         $(this).css({
@@ -22,13 +24,27 @@ $(function(){
         scrollChanges(position);
     });
 
-    //Change timeline active element
+    //Change timeline active element on click
     $('.timeline > a').on('click',function(){
 
         $('.timeline > a').removeClass('timeline-active');
         $(this).addClass('timeline-active');
 
     });
+
+    //Smooth scroll
+    $('.timeline a[href^="#"], nav a[href^="#"]').not('[href="#0"]').on('click',function(event){
+        event.preventDefault();
+
+        var target = $($(this).attr('href'));
+
+        if(target.length)
+            $('html,body').animate({
+                scrollTop: target.offset().top - 64
+            }, 500);
+    });
+
+    //----------FUNCTIONS----------
 
     //Scroll changes
     function scrollChanges(position){

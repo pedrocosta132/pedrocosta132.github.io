0f87a09c1fb955702e84485a58053ac1b67932b8
pedrocosta132 <pedroarcosta99@gmail.com>
2020-02-27 23:36:33 +0000

    'Home' & 'Portfolio' Sections

--- portfolio/assets/js/script.js
+++ portfolio/assets/js/script.js
@@ -6,7 +6,7 @@ $(function(){
     //Make necessary scroll changes on page load
     scrollChanges($(window).scrollTop());
 
-    //----------LISTENERS----------
+    
 
     //Add animation delay to nav links dynamically
     $("header > nav > ul > li").each(function(index){
@@ -14,6 +14,8 @@ $(function(){
              'animation-delay' : (400*index) + 'ms'
         });
     });
+
+    //----------LISTENERS----------
 
     //On scroll actions
     $(window).on('scroll load',function(){
@@ -33,28 +35,83 @@ $(function(){
     });
 
     //Smooth scroll
-    $('.timeline a[href^="#"], nav a[href^="#"]').not('[href="#0"]').on('click',function(event){
+    $('a[href^="#"]').not('[href="#0"]').not('[href="#"]').on('click',function(event){
         event.preventDefault();
 
         var target = $($(this).attr('href'));
 
         if(target.length)
             $('html,body').animate({
-                scrollTop: target.offset().top - 64
+                scrollTop: target.offset().top
             }, 500);
+    });
+
+    //Portfolio section navigation
+    $('#portfolio-nav > a').on('click',function(){
+        var target = $(this).attr('for'),
+            toShow = $('#portfolio > .content[data-target="' + target + '"');
+        
+        if(!toShow.length)
+            //return;
+        console.log('existe');
+        //navigation
+        $('#portfolio-nav > a.active').removeClass('active');
+        $(this).addClass('active');
+        $('#portfolio-selector').animate({'margin-top': $(this).position().top}, 500);
+
+        //content
+        // $('#portfolio > .content').fadeOut('fast').promise().done(function(){
+        //     toShow.css('display','flex').hide().fadeIn('fast');
+        // });
+        $('#portfolio > .content').hide();
+        toShow.css('display','flex').hide().fadeIn(1000);
+
+        //Load repositories
+        var attr = $('#rep-list').attr('done');
+        if(target === 'rep' && (typeof attr === typeof undefined || attr === false))
+            loadRepositories();
     });
 
     //----------FUNCTIONS----------
 
     //Scroll changes
     function scrollChanges(position){
-        console.log(position);
+        //console.log(position);
 
         //Header on scroll
         $('header').toggleClass('scrolled', position > 100);
 
-        //Showing/hiding timeline
-        $('.timeline').toggleClass('active', position > 675);
+        
+    }
+
+    //Load my public repositories from github
+    function loadRepositories(){
+        console.log('cenas');
+        return;
+        $.get(url,
+        null,
+        function(data, status){
+
+            console.log(data);
+
+            var rep_list = $('#rep-list');
+            //data = null;
+            if(status == 'success' && data != null){
+                $('#rep-list img').hide('normal');
+                rep_list.html('');
+                data.forEach(d => {
+                    //add to rep list
+                    if(d.description == null)
+                        d.description = '...';
+                    //rep_list.append(`<li><a target="_blank" href="${d.html_url}">${d.name} <i class="fas fa-link"></i></a><p>${d.description}</p></li>`);
+                    console.log(d.name);
+                });
+            }else
+                rep_list.html('<li>No repository found</li>');  
+
+        }).fail(function(xhr){
+            console.log("Error " + xhr.status + " - " + xhr.statusText);
+        });
     }
 
 });
